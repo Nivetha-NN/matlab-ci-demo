8d2b59c2a7466ceef74312b8d2a2b0cfcfc442da
Nivetha <2021WC86310@wilp.bits-pilani.ac.in>
2025-07-14 22:07:28 +0530

    Updated greeting message in App.java

--- devops-java-demo/src/main/java/com/example/App.java
+++ devops-java-demo/src/main/java/com/example/App.java
@@ -1,5 +1,6 @@
+package com.example;
 public class App {
-   public static void main(String[] args) {
-       System.out.println("Hello from Jenkins CI!");
-   }
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI/CD Pipeline!");
+    }
 }
